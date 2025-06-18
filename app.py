7fb64baf167c866289ecbb196b661cb25bfa40ac
qghop <cschatmeyer@gmail.com>
2025-06-18 12:29:19 -0400

    Added basic timeline view

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from nicegui import ui
 import osxphotos
 from PIL import Image
 import pillow_heif
+import numpy as np
 from datetime import datetime, timedelta
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import helper
 
 pillow_heif.register_heif_opener()
@@ -87,20 +89,84 @@ def timeline_view():
     
     # Get all photos with date
     photos = db.photos()
-    
     timeline_df = pd.DataFrame([
         {
             'date_original': p.date_original,
             'latitude': p.latitude,
             'longitude': p.longitude,
-            'utc_time': p.date_original - timedelta(seconds=p.tzoffset) if p.tzoffset else p.date_original,
+            #'utc_time': p.date_original - timedelta(seconds=p.tzoffset) if p.tzoffset else p.date_original,
         }
         for p in photos if p.date_original and p.latitude and p.longitude
     ])
-
+    
+    # Explicitly convert to datetime, sort
+    timeline_df['date_original'] = pd.to_datetime(timeline_df['date_original'], errors='raise', utc=True)
     timeline_df.sort_values(by='date_original', inplace=True)
     
+    # Set up the Plotly figure
+    fig = go.Figure()
+    fig.update_layout(
+            geo=dict(
+                scope='world',
+                showland=True,
+                showcountries=True,
+                landcolor='rgb(243, 243, 243)',
+                coastlinecolor='rgb(204, 204, 204)',
+                projection_type='natural earth',
+            ),
+        )
     
+    # Plot paths, points for each year
+    years = timeline_df['date_original'].dt.year.unique()
+    year_span = years.max() - years.min()
+    for year in years:
+        year_df = timeline_df[timeline_df['date_original'].dt.year == year]
+        
+        if len(year_df) < 2:
+            continue
+
+        starts = year_df.iloc[:-1]
+        ends = year_df.iloc[1:]
+
+        num_segments = len(starts)
+        lons = np.empty(num_segments * 3)
+        lats = np.empty(num_segments * 3)
+        lons[0::3] = starts['longitude'].values
+        lons[1::3] = ends['longitude'].values
+        lons[2::3] = np.nan
+        lats[0::3] = starts['latitude'].values
+        lats[1::3] = ends['latitude'].values
+        lats[2::3] = np.nan
+
+        color_num = ((year - years.min()) / year_span) * 255
+
+        # Lines
+        fig.add_trace(
+            go.Scattergeo(
+                lon=lons,
+                lat=lats,
+                mode='lines',
+                line=dict(width=2, color=f'rgb({color_num}, 0, {255 - color_num})'),
+                name=f'{year}',
+                opacity=0.4
+            )
+        )
+        
+        # Points
+        fig.add_trace(
+            go.Scattergeo(
+                lon=year_df['longitude'],
+                lat=year_df['latitude'],
+                mode='markers',
+                marker=dict(size=3, color=f'rgb({color_num}, 0, {255 - color_num})'),
+                text=year_df['date_original'].dt.strftime("%b %-d, %Y at %-I:%M %p"),
+                hoverinfo='text',
+                opacity=0.7,
+                showlegend=False,
+            )
+        )
+        
+    ui.plotly(fig).classes('w-full h-[80vh]')
 
 
 ui.run(reload=False)
